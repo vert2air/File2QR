13ccf7942fe8f368b3f9c8d59c8b45da6f4ba9db
vert2air <vert2air@gmail.com>
2025-08-02 12:50:10 +0900

    add : ファイル名のDrag & Drop対応
    On branch feature_dnd_file
    Changes to be committed:
    	modified:   portFile.py

--- portFile.py
+++ portFile.py
@@ -7,6 +7,7 @@ import qrcode
 import re
 import tkinter as Tk
 from tkinter import filedialog
+from tkinterdnd2 import TkinterDnD, DND_FILES
 import tempfile
 from PIL import ImageTk
 import zipfile
@@ -124,6 +125,7 @@ def mergeBase64(ifn):
 # Any File to QRcodes window
 #  Input
 btn_fn = None
+frm_txt_fn = None
 txt_fn = None
 bln_zip = None
 chk_zip = None  # checkbox
@@ -271,8 +273,16 @@ def inMethChange():
         txt_direct.configure(state='disabled')
 
 
+def drop_fn(event=None):
+    if event:
+        file_paths = txt_fn.tk.splitlist(event.data)
+        txt_fn.delete(0, Tk.END)
+        txt_fn.insert(Tk.END, file_paths[0])
+
+
 def gui():
     global btn_fn
+    global frm_txt_fn
     global txt_fn
     global bln_zip
     global chk_zip
@@ -282,7 +292,7 @@ def gui():
     global txt_direct
     global btn_dec
 
-    root = Tk.Tk()
+    root = TkinterDnD.Tk()
     root.geometry('420x200')
     root.title('Any File to QRcodes ')
 
@@ -312,8 +322,12 @@ def gui():
                             variable=str_inMethod, value='file',
                             command=inMethChange)
     rad_fn.pack(side=Tk.LEFT)
+
     txt_fn = Tk.Entry(frm_in_file, width=40)
     txt_fn.pack(side=Tk.LEFT)
+    txt_fn.drop_target_register(DND_FILES)
+    txt_fn.dnd_bind('<<Drop>>', drop_fn)
+
     btn_fn = Tk.Button(frm_in_file, text='Open...', command=file_btn_click)
     btn_fn.pack(side=Tk.LEFT)
 
