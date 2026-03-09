c82ee8ac4e8f4a72f2c33228e98ddb033a1c2891
vert2air <vert2air@gmail.com>
2026-03-09 20:37:10 +0900

    fix: ログを更に抑止。
    環境変数で詳細ログを有効化
    RUST_LOG=debug cargo run
    
    On branch issue_virtual_win11
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() -> eframe::Result<()> {
     env_logger::Builder::from_default_env()
         .filter_level(log::LevelFilter::Info) // デフォルトはInfo以上
         .filter_module("wgpu_hal::vulkan::instance", log::LevelFilter::Off) // Vulkan初期化のみ非表示
+        .filter_module("egui_wgpu", log::LevelFilter::Warn) // egui_wgpuの詳細情報を抑制
         .init();
 
     let options = eframe::NativeOptions {
